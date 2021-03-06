44901f1fe37a79ddb6d130cb968433017f825638
Charles Becker <c.l.becker@outlook.com>
2021-03-06 15:13:44 -0800

    just another commit

---
+++ warmups.R
@@ -0,0 +1,8 @@
+# 6th of 9 courses
+
+library(tidyverse)
+library(dslabs)
+
+MyName <- readline("Okay, so what's your name? ")
+print(paste("Then let's get started, ", MyName, sep=""))
+readline("Tap any key to continue....")
