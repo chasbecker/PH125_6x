d96ca6bf247f7c4050dfa0f9df889e3f50a3169b
Charles Becker <c.l.becker@outlook.com>
2021-03-09 13:27:55 -0800

    msg

--- warmups.R
+++ warmups.R
@@ -1,8 +1,27 @@
 # 6th of 9 courses...
 
 library(tidyverse)
+library(readxl)
 library(dslabs)
 
-MyName <- readline("Okay, so what's your name? ")
-print(paste("Then let's get started, ", MyName, sep=""))
-readline("Tap any key to continue....")
+# MyName <- readline("Okay, so what's your name? ")
+# print(paste("Then let's get started, ", MyName, sep=""))
+# readline("Tap any key to continue on to the course....")
+
+path <- system.file("extdata", package = "dslabs")
+files <- list.files(path)
+files
+
+filename <- "murders.csv"
+murdat <- read_csv(file.path(path, filename))
+murdat
+murdat$region <- as.factor(murdat$region)
+head(murdat)
+rm(list=ls())
+
+
+XlFile <- file.path("C:", "Users", "Charles", "OneDrive", "Desktop", "TempStuff", "NYvUSA.xlsx")
+#alternatively:  XlFile <- ("C:\\Users\\Charles\\OneDrive\\Desktop\\TempStuff\\NYvsUSA.xlsx")
+SheetList <- excel_sheets(XlFile)
+length(SheetList)
+for (i in 1:length(SheetList)){print(paste(i,SheetList[i],sep=": "))}
